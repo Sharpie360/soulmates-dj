990146f973bd3f3977e26a0e4c75cc86967170f8
SeanBecker <techtronixwebdesign@gmail.com>
2019-02-02 09:12:06 -0600

    added - hero-container cmp, removed - helloworld cmp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hero--container></hero--container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Hero__Container from '@/components/hero/Hero__Container.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    'hero--container': Hero__Container
   },
 };
 </script>
