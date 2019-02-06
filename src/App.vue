52c4a95cf8191af29c454cf51ad4cd66e22af09f
SeanBecker <techtronixwebdesign@gmail.com>
2019-02-06 01:05:27 -0600

    updated - minor changes

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
 </template>
 
 <script>
-import Navbar_Container from './components/navbar/Navbar_Container';
+import Navbar__Container from './components/navbar/Navbar__Container';
 
 export default {
   components: {
-    'app--navbar': Navbar_Container,
+    'app--navbar': Navbar__Container,
   }
 }
 </script>
@@ -27,16 +27,4 @@ export default {
   height: 100%;
 }
 
-.navbar-brand {
-  margin-left: 1rem;
-}
-
-#nav a {
-  width: 8rem;
-  color: #f7f7f7;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
