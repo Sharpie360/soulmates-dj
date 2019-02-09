6cd6e99a18f04f6b1c74979f6a062c541a43236c
SeanBecker <techtronixwebdesign@gmail.com>
2019-02-08 23:08:01 -0600

    added - services router view, added services route

--- src/router.js
+++ src/router.js
@@ -19,5 +19,10 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/services',
+      name: 'services',
+      component: () => import(/* webpackChunkName: "services" */'./views/Services.vue'),
+    },
   ],
 });
---
+++ src/views/Services.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="services-view--outer">
+    <h1>Services</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
