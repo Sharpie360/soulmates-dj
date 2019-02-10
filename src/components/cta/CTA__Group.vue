20369f64cbd78b5394e4f61bf7b72968773845c8
SeanBecker <techtronixwebdesign@gmail.com>
2019-02-10 06:53:40 -0600

    refactor - renamed cta cmps, built cta-group, navbar dynamic styling

---
+++ src/components/cta/CTA__Group.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cta--group flexbox">
+    <cta--primary 
+      to="hire" 
+      text="Hire Now!">
+    </cta--primary>
+    <div class="flex-spacer flex-1"></div>
+    <cta--secondary 
+      to="services" 
+      text="Learn More">
+    </cta--secondary>
+  </div>
+</template>
+
+<script>
+import CTA__Primary from './CTA__Primary';
+import CTA_Secondary from './CTA__Secondary';
+
+export default {
+  components: {
+    'cta--primary': CTA__Primary,
+    'cta--secondary': CTA_Secondary
+  }
+}
+</script>
+
+<style scoped>
+.cta--group {
+  position: relative;
+  top: 42rem;
+  width: 40%;
+  margin: 0 auto;
+}
+</style>
--- src/components/cta/CTA__Secondary.vue
+++ src/components/cta/CTA__Secondary.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link class="cta--outer cta--secondary flexbox-space-center" tag="div" 
+  <router-link class="cta--outer cta--secondary flexbox-space-center" tag="button" 
   :to="to">
     <span class="cta--inner">{{ text }}</span>
   </router-link>
@@ -20,12 +20,13 @@ export default {
 }
 .cta--inner {
   text-align: center;
+  font-size: 190%;
 }
 .cta--secondary {
   background-color: transparent;
   border: 2px solid var(--offBlack);
   color: var(--brightRed);
-  font-weight: 600;
+  font-weight: 500;
   transition: background-color .3s;
 }
 .cta--secondary:hover, .cta--secondary:active {
