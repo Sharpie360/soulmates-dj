af89e1db6accd3d494eb466c2dcb1376f8ea8a4f
SeanBecker <techtronixwebdesign@gmail.com>
2019-02-10 07:33:52 -0600

    added - Hire view and route

--- src/router.js
+++ src/router.js
@@ -19,5 +19,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+
+    {
+      path: '/hire',
+      name: 'hire',
+      component: () => import(/* webpackChunkName: "hire" */'./views/Hire.vue'),
+    },
   ],
 });
---
+++ src/views/Hire.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="view--hire--outer">
+    <h1>Hire Now!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
